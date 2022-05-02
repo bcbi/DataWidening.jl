f5087c2964383a16d09b6212102cc16b1fff6516
Documenter.jl <documenter@juliadocs.github.io>
2022-05-02 23:15:03 +0000

    build based on 0376fc0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DataWidening","category":"page"},{"location":"#DataWidening","page":"Home","title":"DataWidening","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DataWidening.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DataWidening]","category":"page"}]
+}
